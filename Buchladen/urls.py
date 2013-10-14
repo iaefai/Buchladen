4617a3747549e1d333f909a9c7c643bc9558e9d8
Jeffrey Drake <jptdrake@gmail.com>
2013-10-14 02:55:02 -0400

    Updated polls application to be done tutorial part 4.

--- Buchladen/urls.py
+++ Buchladen/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'Buchladen.views.home', name='home'),
     # url(r'^Buchladen/', include('Buchladen.foo.urls')),
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
