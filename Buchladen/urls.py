7523b131cc45eb31d532a5b4e864c8f8a562dac1
Jeffrey Drake <drakej@uwindsor.ca>
2013-11-10 22:38:06 -0500

    Moved urls from Project to App

--- Buchladen/urls.py
+++ Buchladen/urls.py
@@ -12,20 +12,7 @@ admin.autodiscover()
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'Buchladen.views.home', name='home'),
-    # url(r'^Buchladen/', include('Buchladen.foo.urls')),
-    url(r'^$', include('store.urls', namespace='store')),
-    url(r'^index', 'store.views.index'),
-    url(r'^login', 'store.views.login_user'),
-    url(r'^booklist', 'store.views.book_list'),
-    url(r'^contact-seller', 'store.views.contact_seller'),
-	url(r'^search/$', 'store.views.search'),
-    url(r'^search/isbn/(?P<isbn_number>[^/]+)', 'store.views.isbn'),
-    url(r'^search/author/(?P<author_name>[^/]+)', 'store.views.author'),
-    url(r'^search/title/(?P<title_name>[^/]+)', 'store.views.title'),
-    url(r'^email-sent', 'store.views.email_send'),
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^', include('store.urls', namespace='store')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     
     # Uncomment the next line to enable the admin:
--- store/urls.py
+++ store/urls.py
@@ -5,14 +5,14 @@ from store import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^/login$', views.login_user, name='login_user'),
-    url(r'^/contact$', views.ContactView.as_view(), name='contact'),
+    #url(r'^login$', views.login_user, name='login_user'),
+    url(r'^contact$', views.ContactView.as_view(), name='contact'),
     #url(r'^/contact-seller$', views.contact_seller, name='contact_seller'),
-	url(r'^/search/$', views.search, name='search'),
-    url(r'^/search/isbn/(?P<isbn_number>[^/]+)$', views.isbn, name='isbn'),
-    url(r'^/search/author/(?P<author_name>[^/]+)', views.author, name='author'),
-    url(r'^/search/title/(?P<title_name>[^/]+)', views.title, name='title'),
-    url(r'^/email-sent/$', views.email_send, name='email_send'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^search/isbn/(?P<isbn_number>[^/]+)$', views.isbn, name='isbn'),
+    url(r'^search/author/(?P<author_name>[^/]+)', views.author, name='author'),
+    url(r'^search/title/(?P<title_name>[^/]+)', views.title, name='title'),
+    url(r'^email-sent/$', views.email_send, name='email_send'),
     # ex: /polls/5/
     #url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
